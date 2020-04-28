4a25f654867dc7646b121b013479aaf544c6a0e5
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2020-04-28 19:42:08 -0400

    Añadiendo nuevas funciones

--- agenda.lua
+++ agenda.lua
@@ -17,18 +17,25 @@ local builder   = Gtk.Builder()
 assert(builder:add_from_file('vistas/agenda.ui'))
 local ui = builder.objects
 
-local login_window = ui.dialog_login
-local main_window  = ui.main_window
+local login_window    = ui.dialog_login
+local window_user     = ui.window_user
+local main_window     = ui.main_window
 
-local btn_cancel   = builder:get_object('btn_cancel')     -- Invoco el objeto btn_cancel de agenda.ui
-local btn_ok       = builder:get_object('btn_ok')         -- Invoco el objeto btn_ok de agenda.ui
+local btn_cancel      = builder:get_object('btn_cancel')         -- Invoco el objeto btn_cancel de agenda.ui
+local btn_ok          = builder:get_object('btn_ok')             -- Invoco el objeto btn_ok de agenda.ui
 
-local input        = builder:get_object('entry_user')     -- Invoco el objeto entry_user de agenda.ui
-local password     = builder:get_object('entry_password') -- Invoco el objeto entry_password de agenda.ui
+local btn_user        = builder:get_object('btn_user')           -- Invoco el objeto btn_user de agenda.ui
+local btn_user_ok     = builder:get_object('btn_user_ok')        -- Invoco el objeto btn_user_ok de agenda.ui
+local btn_user_cancel = builder:get_object('btn_user_cancel')    -- Invoco el objeto btn_user_cancel de agenda.ui
 
-function btn_ok:on_clicked()
-	db:open()
-	local sql = "select id_usuarios from usuarios where usuario = '"..input.text.."' and contrasena = '"..password.text.."'"
+local input           = builder:get_object('entry_user')         -- Invoco el objeto entry_user de agenda.ui
+local password        = builder:get_object('entry_password')     -- Invoco el objeto entry_password de agenda.ui
+local contactos_view  = builder:get_object('contactos_view')     -- Invoco el objeto entry_password de agenda.ui
+
+
+local function aceptar()
+    db:open()
+	local sql = "select id_usuario from usuarios where usuario = '"..input.text.."' and contrasena = '"..password.text.."'"
 	local result = db:get_var(sql)
 	if result then
 		print("Bienvenidos")
@@ -37,6 +44,16 @@ function btn_ok:on_clicked()
 	else
 		print('contraseña o usuario incorrecto')
 	end
+end 
+
+function btn_ok:on_clicked()
+    aceptar()
+end
+
+function ui.entry_password:on_key_release_event(env)
+    if ( env.keyval  == Gdk.KEY_Return ) then
+      aceptar()
+    end
 end
 
 local btn_add   = builder:get_object('btn_registrar') -- Invoco el objeto btn_add de agenda.ui
@@ -46,12 +63,12 @@ local function poblar_lista()
 	db:open()
 	local lista_contactos = builder:get_object('lista_contactos')
 	lista_contactos:clear()
-	local sql = "select * from contactos order by id_contactos desc"
+	local sql = "select * from contactos order by id_contacto desc"
 	local consulta = db:get_results(sql)
 
 	for i,item in pairs(consulta) do
 		lista_contactos:append({
-			item.id_contactos,
+			item.id_contacto,
 			item.nombre,
 			item.numero,
 			item.lugar,
@@ -63,7 +80,7 @@ end
 poblar_lista()
 
 local input_nombre = builder:get_object('entry_nombre') -- Invoco el objeto entry_nombre de agenda.ui
-local input_numero = builder:get_object('entry_numero') -- Invoco el objeto entry_password de agenda.ui
+local input_numero = builder:get_object('entry_numero') -- Invoco el objeto entry_numero de agenda.ui
 local input_lugar  = builder:get_object('entry_lugar')  -- Invoco el objeto entry_lugar de agenda.ui
 
 local function insert_data()
@@ -85,10 +102,59 @@ local function insert_data()
 	end
 end
 
+local function user_exist(user)
+    db:open()
+    local sql = "SELECT usuario from usuarios where usuario='"..user.."'"
+    local resultado = db:get_var(sql)
+    if (resultado) then
+        return true
+    else
+        return false
+    end
+end
+
+local input_usuario    = builder:get_object('entry_user_usuario')    -- Invoco el objeto entry_user_user de agenda.ui
+local input_contrasena = builder:get_object('entry_user_contrasena') -- Invoco el objeto entry_user_contrasena de agenda.ui
+local label_mensaje    = builder:get_object('label_mensaje')         -- Invoco el objeto label_mensaje de agenda.ui
+
+local function insert_user()
+	db:open()
+	local sql = "insert into usuarios(usuario,contrasena,estatus) values('"..input_usuario.text.."','"..input_contrasena.text.."','t')"
+	if input_usuario.text ~= "" and input_contrasena.text ~= "" then
+        if (user_exist(input_usuario.text) == true )then
+            label_mensaje.label = "el usuario existe"
+            return false
+        end
+		if (db:query(sql) == false) then
+			print("error al insertar los datos")
+			print(sql)
+		else
+			input_usuario.text = ""
+			input_contrasena.text = ""
+            label_mensaje.label = "usuario creado correctamente"
+		end
+	else
+		print("error campos vacios")
+	end
+end
+
 function btn_add:on_clicked()
 	insert_data()
 end
 
+function btn_user_ok:on_clicked()
+	insert_user()
+end
+
+function btn_user:on_clicked()
+    ui.window_user:show_all()
+    ui.dialog_login:hide()
+end
+
+function btn_user_cancel:on_clicked()
+	ui.dialog_login:show_all()
+    ui.window_user:hide()
+end
 
 function btn_cancel:on_clicked()
 	Gtk.main_quit()
