8e3847942222195ca8b686a9f1d027f39200dd2d
Victor Diego Alegandro Diaz Urbaneja <sodomon2@gmail.com>
2020-05-24 20:52:37 -0400

    lang english

--- languages/en_us.lua
+++ languages/en_us.lua
@@ -1,4 +1,31 @@
 lang = {
-  @TODO
+  user                 = "User :";
+  password             = "Password :";
+  user_menssage        = "password or user incorrect";
+  acept                = "Acept";
+  cancel               = "Cancel";
+  pass                 = "Password";
+  user_entry           = "User";
+
+  create_user          = "Name :";
+  create_password      = "Password :";
+  user_create          = "Create a user";
+  exist_user           = "User exist";
+  error                = "Error";
+
+  about                = "About";
+  login                = "Please Identifiquer";
+  user_window          = "Create user";
+  window               = "Agenda Personal";
+
+  clear                = "Clear";
+  register             = "Register";
+
+  name                 = "Name";
+  number               = "Number";
+  zone                 = "Zone";
+  insert               = "insert ";
+  exit                 = "Exit to login";
+  close                = "Exit Agenda";
 }
 return lang
